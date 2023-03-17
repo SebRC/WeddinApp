8407aef5ddc37d3b60eb0e410b7b974c46e6197f
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-03-17 18:33:50 +0100

    Add active state to nav bar links

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -1,13 +1,18 @@
 import { FunctionComponent } from "react";
 import styles from "./Navbar.module.css";
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 
 export const Navbar: FunctionComponent = () => {
+  const location = useLocation();
   return (
     <>
       <div className={styles.navbar}>
-        <Link to="guest">Guest</Link>
-        <Link to="admin">Admin</Link>
+        <Link to="guest" className={location.pathname.includes("guest") ? `${styles.active}` : ""}>
+          Guest
+        </Link>
+        <Link to="admin" className={location.pathname.includes("admin") ? `${styles.active}` : ""}>
+          Admin
+        </Link>
       </div>
       <div className={styles.outlet}>
         <Outlet />
