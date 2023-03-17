11adaa3e78a761ddb87b6c9a22a57ac8cbe17df3
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-03-17 15:27:08 +0100

    Add more guests

--- src/guest/guests.ts
+++ src/guest/guests.ts
@@ -1,8 +1,24 @@
 import {Guest} from "./Guest";
 
-export const Guests: Guest[] = [
+export const Guests: Guest[] = 
+[
     {name: "Martin", attending: false, songWishes: ["Lukas Graham - 7 years"], 
-    guests: [{name: "Stine", attending: false, songWishes: ["Jonah Blacksmit - House is on fire"]}, {name: "Liv", attending: false, foodInfo: "Likes milk", songWishes: ["Taylor Swift - Antihero"]},
+    guests: 
+    [
+        {name: "Stine", attending: false, songWishes: ["Jonah Blacksmit - House is on fire"]}, {name: "Liv", attending: false, foodInfo: "Likes milk", songWishes: ["Taylor Swift - Antihero"]},
     ]},
     {name: "Søren", attending: false, foodInfo: "Loves a big beer", songWishes: ["Drake - Gods plan", "Beyonce - Single Ladies"]},
-    {name: "Sebastian Refsbæk Christiansen", attending: false, foodInfo: "Hates onions. NO onions please.", songWishes: ["The Weeknd -  Take my breath", "Mina Okabe - Every second", "Tove Lo - Cool girl"]}];
+    {name: "Sebastian Refsbæk Christiansen", attending: false, foodInfo: "Hates onions. NO onions please.", songWishes: ["The Weeknd -  Take my breath", "Mina Okabe - Every second", "Tove Lo - Cool girl"]},
+    {name: "Lærke Overgaard", attending: false, songWishes: [], 
+    guests: 
+    [
+        {name: "Henrik Faxholm", attending: false, songWishes: []},
+        {name: "Jonathan Faxholm", attending: false, songWishes: []},
+    ]},
+    
+    {name: "Mathias Refsbæk Christiansen", attending: false, songWishes: [], 
+    guests: 
+    [
+        {name: "Line Sølling Refsbæk", attending: false, songWishes: ["Jonah Blacksmit - House is on fire"]},
+    ]},
+];
