f0af2995b4b182d7abd49d75994376d593800ec0
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-03-27 12:45:35 +0200

    Add sorting functionality to guests table

--- src/components/table/TableHeader.tsx
+++ src/components/table/TableHeader.tsx
@@ -1,18 +1,47 @@
 import { FunctionComponent } from "react";
+import { Flexbox } from "../layout/flexbox/Flexbox";
+import { SortOrder } from "./sortOrder";
 import styles from "./TableHeader.module.css";
 
 interface TableHeaderProps {
-  headers: { name: string; width: any }[];
+  headers: {
+    name: string;
+    width: string;
+    sortable?: boolean;
+    sorted?: SortOrder;
+    onSort?: () => void;
+  }[];
 }
 
 export const TableHeader: FunctionComponent<TableHeaderProps> = ({ headers }) => {
+  const getSortIcon = (currentState: SortOrder) => {
+    if (currentState === SortOrder.Unsorted) {
+      return (
+        <Flexbox flexDirection="column" justifyContent="space-between">
+          <div className={styles.sortableUp} />
+          <div className={styles.sortableDown} />
+        </Flexbox>
+      );
+    }
+    if (currentState === SortOrder.Ascending) {
+      return <div className={styles.sortableUp} />;
+    }
+    return <div className={styles.sortableDown} />;
+  };
   return (
     <thead className={styles.header}>
       <tr>
         {headers.map((h, index) => {
           return (
             <th key={h.name + index} style={{ width: h.width }}>
-              {h.name}
+              <Flexbox gap={5} alignItems="center">
+                {h.name}
+                {h.sortable && (
+                  <div className={styles.sortableWrapper} onClick={() => h.onSort?.()}>
+                    {getSortIcon(h.sorted ?? SortOrder.Unsorted)}
+                  </div>
+                )}
+              </Flexbox>
             </th>
           );
         })}
--- src/components/table/guest/GuestTable.tsx
+++ src/components/table/guest/GuestTable.tsx
@@ -1,26 +1,56 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 import { Guest } from "../../../guest/Guest";
 import styles from "../Table.module.css";
 import { GuestTableRow } from "./GuestTableRow";
 import { TableHeader } from "../TableHeader";
+import { SortOrder } from "../sortOrder";
 
 interface GuestTableProps {
   guests: Guest[];
 }
 
 export const GuestTable: FunctionComponent<GuestTableProps> = ({ guests }) => {
+  const [sortedGuest, setSortedGuests] = useState(guests.slice());
+  const [sorted, setSorted] = useState(SortOrder.Unsorted);
+  const getNextSortState = () => {
+    if (sorted === SortOrder.Unsorted) {
+      return SortOrder.Ascending;
+    }
+    if (sorted === SortOrder.Ascending) {
+      return SortOrder.Descending;
+    }
+    return SortOrder.Unsorted;
+  };
+
+  const handleSort = () => {
+    const nextSortState = getNextSortState();
+    setSorted(nextSortState);
+    if (nextSortState === SortOrder.Unsorted) {
+      setSortedGuests(guests.slice());
+    } else if (nextSortState === SortOrder.Ascending) {
+      setSortedGuests(sortedGuest.sort((a, b) => Number(b.attending) - Number(a.attending)));
+    } else {
+      setSortedGuests(sortedGuest.sort((a, b) => Number(a.attending) - Number(b.attending)));
+    }
+  };
   return (
     <table className={styles.table}>
       <TableHeader
         headers={[
           { name: "Navn", width: "20%" },
-          { name: "Kommer", width: "10%" },
+          {
+            name: "Kommer",
+            width: "10%",
+            sortable: true,
+            sorted: sorted,
+            onSort: handleSort,
+          },
           { name: "Song wishes", width: "40%" },
           { name: "Food info", width: "30%" },
         ]}
       />
       <tbody>
-        {guests.map((g) => {
+        {sortedGuest.map((g) => {
           return g.guests ? (
             g.guests
               .map((gg, index) => {
