ccce8e32ebfa9825241245e56de9a0f658cb0f77
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-04-01 09:16:25 +0200

    Use translator in all gift components

--- src/components/info/DateAndTime.tsx
+++ src/components/info/DateAndTime.tsx
@@ -1,14 +1,13 @@
 import { FunctionComponent } from "react";
+import { useTranslator } from "../../translations/useTranslator";
 import { Header } from "../text/Header";
 import { TimeTable } from "./TimeTable";
 
 export const DateAndTime: FunctionComponent = () => {
+  const translator = useTranslator();
   return (
     <>
-      <Header
-        text="Hvornår"
-        subHeader="Vi har valgt lørdag den 27. juli 2024 kl. 14:30 til at fejre vores kærlighed."
-      />
+      <Header text={translator.when()} subHeader={translator.weddingDateDescription()} />
       <TimeTable />
     </>
   );
