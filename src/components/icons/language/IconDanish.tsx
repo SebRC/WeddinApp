b18155f4a52f7ea2dcde240f23cc28b3a916a0d6
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-04-01 15:30:23 +0200

    Adjust icon danish and icon english

--- src/components/icons/language/IconDanish.tsx
+++ src/components/icons/language/IconDanish.tsx
@@ -7,7 +7,7 @@ export const IconDanish: FunctionComponent = () => {
     <svg width={size} height={size} viewBox="0 0 413 267" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="2.5" y="2.5" width="408" height="262" rx="27.5" fill="#C02121" stroke="black" stroke-width="5" />
       <rect x="123" y="5" width="46" height="257" fill="white" />
-      <rect x="5" y="152" width="46" height="403" transform="rotate(-90 5 152)" fill="white" />
+      <rect x="5" y="156" width="46" height="403" transform="rotate(-90 5 156)" fill="white" />
     </svg>
   );
 };
--- src/components/icons/language/IconEnglish.tsx
+++ src/components/icons/language/IconEnglish.tsx
@@ -7,7 +7,7 @@ export const IconEnglish: FunctionComponent = () => {
     <svg width={size} height={size} viewBox="0 0 413 267" fill="none" xmlns="http://www.w3.org/2000/svg">
       <rect x="2.5" y="2.5" width="408" height="262" rx="27.5" fill="white" stroke="black" stroke-width="5" />
       <rect x="123" y="5" width="46" height="257" fill="#B11414" />
-      <rect x="5" y="152" width="46" height="403" transform="rotate(-90 5 152)" fill="#AC1616" />
+      <rect x="5" y="156" width="46" height="403" transform="rotate(-90 5 156)" fill="#AC1616" />
     </svg>
   );
 };
