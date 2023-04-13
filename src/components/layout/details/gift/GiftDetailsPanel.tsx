870eed663cdfa50d0a17a902fe087abbbac20961
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-04-13 21:13:15 +0200

    Show image of gift in details panel

--- src/components/layout/details/gift/GiftDetailsPanel.tsx
+++ src/components/layout/details/gift/GiftDetailsPanel.tsx
@@ -1,9 +1,12 @@
-import { FunctionComponent, useState } from "react";
+import { FunctionComponent, useEffect, useState } from "react";
+import { getImage } from "../../../../firebase/firebase";
 import { useTranslator } from "../../../../translations/useTranslator";
 import { Button } from "../../../button/Button";
 import { Gift } from "../../../gift/gift";
 import { IconCheckmark } from "../../../icons/IconCheckmark";
 import { IconX } from "../../../icons/IconX";
+import { Image } from "../../../image/Image";
+import { LoadingImage } from "../../../loading/LoadingImage";
 import { DeleteGiftModal } from "../../../modal/gift/DeleteGiftModal";
 import { Header } from "../../../text/Header";
 import { Flexbox } from "../../flexbox/Flexbox";
@@ -16,6 +19,8 @@ interface GiftDetailsPanelProps {
 
 export const GiftDetailsPanel: FunctionComponent<GiftDetailsPanelProps> = ({ gift, onClose }) => {
   const [showModal, setShowModal] = useState(false);
+  const [loadingImage, setLoadingImage] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
   const translator = useTranslator();
 
   const handleDelete = () => {
@@ -23,13 +28,25 @@ export const GiftDetailsPanel: FunctionComponent<GiftDetailsPanelProps> = ({ gif
     setShowModal(false);
   };
 
+  useEffect(() => {
+    (async () => {
+      setLoadingImage(true);
+      const url = await getImage(gift.image);
+      setImageUrl(url);
+      setLoadingImage(false);
+    })();
+  }, [gift.image]);
+
   return (
     <DetailsPanel title={gift.name} onClose={onClose}>
       <Flexbox flexDirection="column" gap={10}>
         <Header text={translator.reserved()} />
-        <Flexbox gap={20}>
-          {gift.reserved ? <IconCheckmark /> : <IconX />}
-          {gift.reserved ? translator.yes() : translator.no()}
+        <Flexbox justifyContent="space-between">
+          <Flexbox gap={20}>
+            {gift.reserved ? <IconCheckmark /> : <IconX />}
+            {gift.reserved ? translator.yes() : translator.no()}
+          </Flexbox>
+          {loadingImage ? <LoadingImage size="150px" /> : <Image url={imageUrl} size="150px" />}
         </Flexbox>
       </Flexbox>
       <Header
