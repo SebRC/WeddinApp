f4d5f916edf28db688e08fbf96de3211b204497a
Sebastian Refsbæk Christiansen <seb-chris@live.dk>
2023-04-13 12:42:13 +0200

    Create function for uploading image to firebase

--- src/components/input/file/FileInput.tsx
+++ src/components/input/file/FileInput.tsx
@@ -1,28 +1,51 @@
 import { FunctionComponent, useState } from "react";
+import { createGift } from "../../../firebase/firebase";
+import { useTranslator } from "../../../translations/useTranslator";
+import { Button } from "../../button/Button";
 import { IconUpload } from "../../icons/IconUpload";
 import { Flexbox } from "../../layout/flexbox/Flexbox";
+import { Header } from "../../text/Header";
 import styles from "./FileInput.module.css";
 export const FileInput: FunctionComponent = () => {
-  const [file, setFile] = useState("");
+  const [file, setFile] = useState<File>();
+  const [percentage, setPercentage] = useState(0);
+
+  const handleProgress = (progress: number) => {
+    setPercentage(progress);
+  };
+
+  const handleUpload = () => {
+    if (file) {
+      createGift(file, handleProgress);
+    }
+  };
+
+  const translator = useTranslator();
 
   return (
-    <Flexbox>
-      <div className={styles.fileText}>{file ? file : "No file chosen"}</div>
-      <div className={styles.container}>
-        <input
-          id="file"
-          type="file"
-          accept="image/*"
-          className={styles.file}
-          onChange={(e) => {
-            setFile(e.target.value);
-          }}
-        />
-        <label htmlFor="file" className={styles.label}>
-          Select file
-          <IconUpload fill="black" />
-        </label>
-      </div>
-    </Flexbox>
+    <>
+      <Flexbox>
+        <div className={styles.fileText}>{file ? file.name : translator.noFileChosen()}</div>
+        <div className={styles.container}>
+          <input
+            id="file"
+            type="file"
+            accept="image/*"
+            className={styles.file}
+            onChange={(e) => {
+              if (e.target.files) {
+                setFile(e.target.files[0]);
+              }
+            }}
+          />
+          <label htmlFor="file" className={styles.label}>
+            {translator.selectFile()}
+            <IconUpload fill="black" />
+          </label>
+        </div>
+      </Flexbox>
+      <Header text={`Progress: ${percentage}%`} />
+      <Button onClick={handleUpload} text="start upload" />
+    </>
   );
 };
--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -1,24 +1,34 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
-import { collection, deleteDoc, doc, DocumentSnapshot, getDoc, getDocs, getFirestore, setDoc, SnapshotOptions } from "firebase/firestore";
+import {
+  collection,
+  deleteDoc,
+  doc,
+  DocumentSnapshot,
+  getDoc,
+  getDocs,
+  getFirestore,
+  setDoc,
+  SnapshotOptions,
+} from "firebase/firestore";
 import { Gift } from "../components/gift/gift";
 import { Guest } from "../components/guest/Guest";
 import { DEFAULT_GUEST_STATE } from "../components/guest/guests";
-import { getStorage, ref, getDownloadURL } from "firebase/storage";
+import { getStorage, ref, getDownloadURL, uploadBytesResumable } from "firebase/storage";
 import { getFunctions, httpsCallable } from "firebase/functions";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
@@ -26,145 +36,200 @@ const app = initializeApp(firebaseConfig);
 
 const functions = getFunctions(app, "europe-west3");
 
-const db = getFirestore(app);
+const storage = getStorage(app);
 
+const db = getFirestore(app);
 export const database = db;
 
 export const getGuest = async (guestId: string): Promise<Guest> => {
-    const docRef = doc(db, "guests", guestId).withConverter(guestConverter);
-    const docSnap = await getDoc(docRef);
-    if (docSnap.exists()) {
-        let docData = docSnap.data();
-        let guests:Guest[] = [];
-        if(docData.guestIds) {
-          guests = await fetchGuestsFromMainGuest(docData.guestIds);
-        }
-        return {...docData, guests: guests};
-    } else {
-      // doc.data() will be undefined in this case
-      console.log("No such document!");
-      return DEFAULT_GUEST_STATE;
+  const docRef = doc(db, "guests", guestId).withConverter(guestConverter);
+  const docSnap = await getDoc(docRef);
+  if (docSnap.exists()) {
+    let docData = docSnap.data();
+    let guests: Guest[] = [];
+    if (docData.guestIds) {
+      guests = await fetchGuestsFromMainGuest(docData.guestIds);
     }
-}
+    return { ...docData, guests: guests };
+  } else {
+    // doc.data() will be undefined in this case
+    console.log("No such document!");
+    return DEFAULT_GUEST_STATE;
+  }
+};
 
 export const getAllGuests = async (): Promise<Guest[]> => {
   const querySnapshot = await getDocs(collection(db, "guests").withConverter(guestConverter));
-  let guests: Guest[] = []
+  let guests: Guest[] = [];
   querySnapshot.forEach((doc) => {
     guests.push(doc.data());
   });
   return guests;
-}
+};
 
 export const setGuestData = async (guest: Guest) => {
   const ref = doc(db, "guests", guest.id ?? "undefined").withConverter(guestConverter);
-  const updatedGuest: Guest = {name: guest.name, attending: guest.attending, foodInfo: guest.foodInfo, songWishes: guest.songWishes, guestIds: guest.guestIds}
+  const updatedGuest: Guest = {
+    name: guest.name,
+    attending: guest.attending,
+    foodInfo: guest.foodInfo,
+    songWishes: guest.songWishes,
+    guestIds: guest.guestIds,
+  };
   await setDoc(ref, updatedGuest);
-}
+};
 
 export const createUser = async (email: string, password: string) => {
-    const createWeddingGuest = httpsCallable(functions, 'create-wedding-guest');
-    const result:any = await createWeddingGuest({ email: email , password: password});
-    const data = result.data;
-    return {success: data.success, userId: data.userId, errorCode: data.errorCode, errorMessage: data.errorMessage}
-}
+  const createWeddingGuest = httpsCallable(functions, "create-wedding-guest");
+  const result: any = await createWeddingGuest({ email: email, password: password });
+  const data = result.data;
+  return { success: data.success, userId: data.userId, errorCode: data.errorCode, errorMessage: data.errorMessage };
+};
 
-export const createGuest = async (data: {name: string, id: string, guestNames: string[]}) => {
+export const createGuest = async (data: { name: string; id: string; guestNames: string[] }) => {
   const guestIds = getGuestIds(data.guestNames);
-  var guest: Guest = {name: data.name, guestIds: guestIds, attending: false, songWishes: [], foodInfo: ""};
+  var guest: Guest = { name: data.name, guestIds: guestIds, attending: false, songWishes: [], foodInfo: "" };
   await setDoc(doc(db, "guests", data.id), guest);
   data.guestNames.forEach(async (gn) => {
-    const plusOne: Guest = {name: gn, guestIds: [], attending: false, songWishes: [], foodInfo: ""};
+    const plusOne: Guest = { name: gn, guestIds: [], attending: false, songWishes: [], foodInfo: "" };
     await setDoc(doc(db, "guests", getGuestId(gn)), plusOne);
   });
-}
+};
 
 export const deleteUser = async (userId: string) => {
-  const deleteWeddingGuest = httpsCallable(functions, 'delete-wedding-guest');
-  const result:any = await deleteWeddingGuest({ userId: userId});
+  const deleteWeddingGuest = httpsCallable(functions, "delete-wedding-guest");
+  const result: any = await deleteWeddingGuest({ userId: userId });
   const data = result.data;
-  return {success: data.success, userId: data.userId, errorCode: data.errorCode, errorMessage: data.errorMessage}
-}
+  return { success: data.success, userId: data.userId, errorCode: data.errorCode, errorMessage: data.errorMessage };
+};
 
 export const deleteGuest = async (guest: Guest) => {
   await deleteDoc(doc(db, "guests", guest.id ?? ""));
-  if(guest.guestIds) {
+  if (guest.guestIds) {
     guest.guestIds.map(async (g) => {
-      await deleteDoc(doc(db, "guests", g))
-    })
+      await deleteDoc(doc(db, "guests", g));
+    });
   }
-}
+};
+
+export const createGift = (file: File, handleProgress: (percent: number) => void) => {
+  if (!file) {
+    alert("Please choose a file first!");
+  }
+
+  const storageRef = ref(storage, file.name);
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      const percent = Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100);
+
+      // update progress
+      handleProgress(percent);
+    },
+    (err) => console.log(err),
+    () => {
+      // download url
+      getDownloadURL(uploadTask.snapshot.ref).then((url) => {
+        console.log(url);
+      });
+    }
+  );
+};
 
 const getGuestIds = (guestNames: string[]) => {
-  return guestNames.map(gn => {
+  return guestNames.map((gn) => {
     return getGuestId(gn);
-  })
-}
+  });
+};
 
 const getGuestId = (guestName: string) => {
-    return guestName.split(" ")[0].toLowerCase();
-}
+  return guestName.split(" ")[0].toLowerCase();
+};
 
 const fetchGuestsFromMainGuest = async (guestIds: string[]): Promise<Guest[]> => {
   let guests: Guest[] = [];
-  await Promise.all(guestIds.map(async (g) => {
-    const guestRef = doc(db, "guests", g).withConverter(guestConverter);
-    const guestDocSnap = await getDoc(guestRef);
-    guests.push(guestDocSnap.data() ?? DEFAULT_GUEST_STATE);
-  }));
+  await Promise.all(
+    guestIds.map(async (g) => {
+      const guestRef = doc(db, "guests", g).withConverter(guestConverter);
+      const guestDocSnap = await getDoc(guestRef);
+      guests.push(guestDocSnap.data() ?? DEFAULT_GUEST_STATE);
+    })
+  );
   return guests;
-}
+};
 
 export const getAllGifts = async (): Promise<Gift[]> => {
   const querySnapshot = await getDocs(collection(db, "gifts").withConverter(giftConverter));
-  let gifts: Gift[] = []
+  let gifts: Gift[] = [];
   querySnapshot.forEach((doc) => {
     gifts.push(doc.data());
   });
   return gifts;
-}
+};
 
 export const setGiftData = async (gift: Gift) => {
   const ref = doc(db, "gifts", gift.id ?? "undefined").withConverter(giftConverter);
-  const updatedGift: Gift = {name: gift.name, url: gift.url, price: gift.price, reserved: gift.reserved, reservedBy: gift.reservedBy, image: gift.image}
+  const updatedGift: Gift = {
+    name: gift.name,
+    url: gift.url,
+    price: gift.price,
+    reserved: gift.reserved,
+    reservedBy: gift.reservedBy,
+    image: gift.image,
+  };
   await setDoc(ref, updatedGift);
-}
+};
 
 export const getImage = async (name: string) => {
   // Create a reference to the file we want to download
   const storage = getStorage();
   const imageRef = ref(storage, name);
   return await getDownloadURL(imageRef);
-}
+};
 
 // Firestore data converter
 export const guestConverter = {
   toFirestore: (guest: Guest) => {
-      return {...guest, 
-          attending: guest.attending,
-          foodInfo: guest.foodInfo,
-          songWishes: guest.songWishes,
-          guestIds: guest.guestIds ?? null,
-          };
+    return {
+      ...guest,
+      attending: guest.attending,
+      foodInfo: guest.foodInfo,
+      songWishes: guest.songWishes,
+      guestIds: guest.guestIds ?? null,
+    };
   },
   fromFirestore: (snapshot: DocumentSnapshot, options: SnapshotOptions): Guest => {
-      const data = snapshot.data(options);
-      return {id: snapshot.id, name: data?.name, attending: data?.attending, songWishes: data?.songWishes, foodInfo: data?.foodInfo, guestIds: data?.guestIds}
-  }
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      name: data?.name,
+      attending: data?.attending,
+      songWishes: data?.songWishes,
+      foodInfo: data?.foodInfo,
+      guestIds: data?.guestIds,
+    };
+  },
 };
 
 // Firestore data converter
 export const giftConverter = {
   toFirestore: (gift: Gift) => {
-      return {...gift, 
-          reserved: gift.reserved,
-          reservedBy: gift.reservedBy,
-          };
+    return { ...gift, reserved: gift.reserved, reservedBy: gift.reservedBy };
   },
   fromFirestore: (snapshot: DocumentSnapshot, options: SnapshotOptions): Gift => {
-      const data = snapshot.data(options);
-      return {id: snapshot.id, name: data?.name, url: data?.url, price: data?.price, reserved: data?.reserved, reservedBy: data?.reservedBy, image: data?.image}
-  }
+    const data = snapshot.data(options);
+    return {
+      id: snapshot.id,
+      name: data?.name,
+      url: data?.url,
+      price: data?.price,
+      reserved: data?.reserved,
+      reservedBy: data?.reservedBy,
+      image: data?.image,
+    };
+  },
 };
 
 const auth = getAuth();
@@ -172,17 +237,17 @@ const auth = getAuth();
 export const handleSignIn = async (email: string, password: string): Promise<LoginDetails> => {
   try {
     const userCredentials = await signInWithEmailAndPassword(auth, email, password);
-    return {success: true, userId: userCredentials.user.uid};
-  } catch(error: any){
-    return {success: false, errorCode: error.code, errorMessage: error.message}
+    return { success: true, userId: userCredentials.user.uid };
+  } catch (error: any) {
+    return { success: false, errorCode: error.code, errorMessage: error.message };
   }
 };
 
-export const handleSignOut = async ()  => {
+export const handleSignOut = async () => {
   try {
     await signOut(auth);
-  } catch(error: any){
-    console.log(error)
+  } catch (error: any) {
+    console.log(error);
   }
 };
 
